73fa385ac0b4e4d71bbeaf5d671c00f3ae5f7bc3
toast <toastts@icloud.com>
2021-01-28 04:19:14 -0700

    functional but unstyled/undesigned privacy filter

--- scenes/SceneFilesFolder.js
+++ scenes/SceneFilesFolder.js
@@ -1,11 +1,15 @@
 import * as React from "react";
+import * as Constants from "~/common/constants";
 import * as SVG from "~/common/svg";
 import * as Events from "~/common/custom-events";
 
+import { css } from "@emotion/react";
+import { PopoverNavigation } from "~/components/system/components/PopoverNavigation";
 import { ButtonPrimary } from "~/components/system/components/Buttons";
 import { FileTypeGroup } from "~/components/core/FileTypeIcon";
 import { TabGroup, PrimaryTabGroup, SecondaryTabGroup } from "~/components/core/TabGroup";
 import { GlobalCarousel } from "~/components/system/components/GlobalCarousel";
+import { getPublicAndPrivateFiles } from "~/common/utilities";
 
 import ScenePage from "~/components/core/ScenePage";
 import DataView from "~/components/core/DataView";
@@ -15,9 +19,53 @@ import EmptyState from "~/components/core/EmptyState";
 
 const POLLING_INTERVAL = 10000;
 
+const STYLES_FILTERS_CONTAINER = css`
+  height: 60px;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  align-items: flex-start;
+`;
+
+const STYLES_FILTER_BUTTON = css`
+  padding: 11px;
+  border-radius: 4px;
+  border: 1px solid ${Constants.system.bgGray};
+  color: ${Constants.system.textGray};
+  margin-right: 8px;
+  display: flex;
+  align-items: center;
+  font-size: ${Constants.typescale.lvlN1};
+  cursor: pointer;
+  letter-spacing: -0.1px;
+`;
+
 export default class SceneFilesFolder extends React.Component {
   state = {
     view: 0,
+    scopeTooltip: false,
+    filetypeTooltip: false,
+    filters: [],
+    files: this.props.viewer.library[0].children,
+  };
+
+  _handleScopeTooltip = () => {
+    this.setState({ scopeTooltip: !this.state.scopeTooltip });
+  };
+
+  _handleScopeFilter = (filter) => {
+    let viewer = this.props.viewer;
+    let filtered = getPublicAndPrivateFiles({ viewer });
+
+    if (filter === "all") {
+      this.setState({ files: this.props.viewer.library[0]?.children });
+    } else if (filter === "public") {
+      this.setState({ files: filtered.publicFiles });
+    } else if (filter === "private") {
+      this.setState({ files: filtered.privateFiles });
+    } else {
+      console.log("this is the worst possible scenario");
+    }
   };
 
   componentDidMount = () => {
@@ -28,6 +76,7 @@ export default class SceneFilesFolder extends React.Component {
         index = page.index;
       } else {
         let library = this.props.viewer.library[0]?.children || [];
+        this.setState({ files: library });
         for (let i = 0; i < library.length; i++) {
           let obj = library[i];
           if ((obj.cid && obj.cid === page?.cid) || (obj.id && obj.id === page?.fileId)) {
@@ -117,11 +166,67 @@ export default class SceneFilesFolder extends React.Component {
             </ButtonPrimary>
           }
         />
-        {this.props.viewer.library[0].children && this.props.viewer.library[0].children.length ? (
+        <div css={STYLES_FILTERS_CONTAINER} style={{ postion: "absolute" }}>
+          <div css={STYLES_FILTER_BUTTON} onClick={this._handleScopeTooltip}>
+            privacy
+            {this.state.scopeTooltip ? (
+              <PopoverNavigation
+                style={{
+                  right: 42,
+                  top: 530,
+                  borderColor: Constants.system.bgGray,
+                  color: Constants.system.textGray,
+                  width: 100,
+                }}
+                navigation={[
+                  {
+                    text: (
+                      <span
+                        style={{
+                          color: this.state.privacy === "all" ? Constants.system.brand : null,
+                        }}
+                      >
+                        All
+                      </span>
+                    ),
+                    onClick: () => this._handleScopeFilter("all"),
+                  },
+                  {
+                    text: (
+                      <span
+                        style={{
+                          color:
+                            this.state.privacy === "public" ? Constants.system.brand : "inherit",
+                        }}
+                      >
+                        public
+                      </span>
+                    ),
+                    onClick: () => this._handleScopeFilter("public"),
+                  },
+                  {
+                    text: (
+                      <span
+                        style={{
+                          color:
+                            this.state.privacy === "private" ? Constants.system.brand : "inherit",
+                        }}
+                      >
+                        private
+                      </span>
+                    ),
+                    onClick: () => this._handleScopeFilter("private"),
+                  },
+                ]}
+              />
+            ) : null}
+          </div>
+        </div>
+        {this.state.files?.length ? (
           <DataView
             onAction={this.props.onAction}
             viewer={this.props.viewer}
-            items={this.props.viewer.library[0].children}
+            items={this.state.files}
             onUpdateViewer={this.props.onUpdateViewer}
             view={this.state.view}
           />
